2614fe10dafac39039ce1fb0e6e64009b43cd367
Manuel Schröder <manuel.schroeder@storyblok.com>
2023-05-15 15:23:43 +0200

    style store finder

---
+++ storyblok/ecommerce/Store.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    class="w-full max-w-md lg:max-w-none h-full flex flex-col"
+    v-editable="blok"
+  >
+    <img
+      v-if="blok.image.filename"
+      :src="blok.image.filename + '/m/900x600'"
+      :alt="blok.name"
+      class="mx-auto mb-6 pointer-events-none rounded-lg shadow-lg"
+    />
+    <div class="grow text-dark">
+      <div>
+        <h3 class="font-serif mb-2 text-xl">
+          {{ blok.name }}
+        </h3>
+        {{ blok.description }}
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({ blok: Object })
+</script>
