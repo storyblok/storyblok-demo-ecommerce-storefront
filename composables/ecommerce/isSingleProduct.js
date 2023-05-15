4979c4608012c190aa1f7095d9a36ee8c3239efa
Manuel Schröder <manuel.schroeder@storyblok.com>
2023-05-15 10:38:44 +0200

    add product page overview component

--- composables/ecommerce/isSingleProduct.js
+++ composables/ecommerce/isSingleProduct.js
@@ -1,5 +1,5 @@
 export default async function () {
   const slug = await getSlug()
-  if (!slug[0]) return false
-  return slug[0] === 'products'
+  if (slug.length < 2) return false
+  return slug[0] === 'products' && slug[1] !== 'products'
 }
---
+++ storyblok/ecommerce/ProductPageOverview.vue
@@ -0,0 +1,25 @@
+<script setup>
+const props = defineProps({ blok: Object })
+
+const gridClasses = computed(() => getGridClasses(props.blok.cols))
+</script>
+
+<template>
+  <section
+    class="page-section product-overview"
+    :class="'bg-' + blok.background_color"
+    v-editable="blok"
+  >
+    <div class="container">
+      <Headline
+        v-if="blok.headline"
+        :color="blok.background_color === 'dark' ? 'white' : 'dark'"
+        >{{ blok.headline }}</Headline
+      >
+      <Lead v-if="blok.lead">
+        {{ blok.lead }}
+      </Lead>
+      display products with filters
+    </div>
+  </section>
+</template>
