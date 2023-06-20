21786dfff5fe7b318793b17c4ef231eb4d5a76fd
Manuel Schröder <manuel.schroeder@storyblok.com>
2023-06-20 17:27:35 +0200

    SingleProduct style

--- components/ecommerce/SingleProduct.vue
+++ components/ecommerce/SingleProduct.vue
@@ -18,54 +18,73 @@ const { pending, data: ecommerceProduct } = useLazyAsyncData(
 
 watch(ecommerceProduct, (newEcommerceProduct) => {
   product = newEcommerceProduct
-  //console.log(newEcommerceProduct)
 })
 
 const { addToCart } = useCart()
 
-
+const button = {
+  size: 'default',
+  style: 'default',
+  button_color: 'white',
+}
 </script>
 <template>
-  <StoryblokComponent v-for="blok in blok.bloks_above" :key="blok._uid" :blok="blok" />
+  <StoryblokComponent
+    v-for="blok in blok.bloks_above"
+    :key="blok._uid"
+    :blok="blok"
+  />
   <section v-editable="blok" class="page-section single-product bg-dark">
     <LoadingSpinner v-if="pending" />
-    <div v-else class="container grid lg:grid-cols-2 gap-6 sm:gap-10 md:gap-12 items-center">
-      <div v-if="product.images">
-        <div>
-          <img
-            class="rounded-lg shadow-2xl pointer-events-none w-full max-w-md lg:max-w-full aspect-square lg:aspect-auto object-cover"
-            :src="product.images[0].file.url" :alt="product.name" />
-        </div>
-        <div class="text-left">
-          <Headline color="white" class="text-left" v-if="blok.headline">{{
-            blok.headline
-          }}</Headline>
-          <RichText :text="blok.description" class="prose-invert" />
-          <div class="mt-12">
-            <Headline color="white" size="small">{{ product.name }}</Headline>
-            <div class="prose prose-invert">
-              <ul>
-                <li v-for="benefit in product.content.product_benefits">
-                  {{ benefit.text }}
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div class="flex items-center space-x-8 mt-12">
-            <PriceWithCurrency v-if="product.price && product.currency" :price="String(product.price)"
-              :currency="product.currency" class="text-white text-xl font-serif" />
-            <!--TODO: style add to cart button-->
-            <button @click.prevent="addToCart(product.id)">Add to cart</button>
-          </div>
-        </div>
+    <div
+      v-else-if="!pending && product.images"
+      class="container grid lg:grid-cols-2 gap-6 sm:gap-10 md:gap-12 items-start"
+    >
+      <div>
+        <img
+          class="rounded-lg shadow-2xl pointer-events-none w-full max-w-md lg:max-w-full aspect-square lg:aspect-auto object-cover"
+          :src="product.images[0].file.url"
+          :alt="product.name"
+        />
       </div>
-      <div class="" v-else>
-        <Headline color="white" class="text-left">
-          Product <span class="text-zinc-400">{{ productSlug }}</span> doesn't exist
-        </Headline>
-
+      <div class="text-left">
+        <Headline color="white" class="text-left" v-if="blok.headline">{{
+          blok.headline
+        }}</Headline>
+        <RichText :text="blok.description" class="prose-invert" />
+        <div class="mt-12">
+          <Headline color="white" size="small">{{ product.name }}</Headline>
+          <div class="prose prose-invert">
+            <ul>
+              <li v-for="benefit in product.content.product_benefits">
+                {{ benefit.text }}
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="flex items-center space-x-8 mt-12">
+          <PriceWithCurrency
+            v-if="product.price && product.currency"
+            :price="String(product.price)"
+            :currency="product.currency"
+            class="text-white text-xl font-serif"
+          />
+          <Button @click.prevent="addToCart(product.id)" :button="button"
+            >Add to cart</Button
+          >
+        </div>
       </div>
     </div>
+    <div v-else>
+      <Headline color="white" class="text-left">
+        Product <span class="text-zinc-400">{{ productSlug }}</span> doesn't
+        exist
+      </Headline>
+    </div>
   </section>
-  <StoryblokComponent v-for="blok in blok.bloks_below" :key="blok._uid" :blok="blok" />
+  <StoryblokComponent
+    v-for="blok in blok.bloks_below"
+    :key="blok._uid"
+    :blok="blok"
+  />
 </template>
