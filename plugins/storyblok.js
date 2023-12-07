65f2e13c111e4e8738ac3acb85e96147c4993af2
Manuel Schröder <manuel.schroeder@storyblok.com>
2023-12-07 12:41:34 +0100

    debug token

--- plugins/storyblok.js
+++ plugins/storyblok.js
@@ -5,6 +5,8 @@ export default defineNuxtPlugin(({ vueApp }) => {
   const config = useRuntimeConfig()
   const route = useRoute()
 
+  console.log(route.query)
+
   vueApp.use(StoryblokVue, {
     ...config.public.storyblok,
     accessToken: route.query.token || config.public.templateToken,
