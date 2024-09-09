eb0f8d829a0f6216bbd9a52315cdd41a95177e9f
Manuel Schröder <manuel.schroeder@storyblok.com>
2024-09-09 10:14:15 +0200

    fix i18n issue with products and product categories

--- composables/ecommerce/isSingleProduct.js
+++ composables/ecommerce/isSingleProduct.js
@@ -1,5 +1,16 @@
 export default async function () {
-  const slug = await getSlug()
+  let slug = await getSlug()
+
+  const language = await getLanguage(slug)
+  /**
+   * If a specific language is requested, the first part of the slug (the language code) needs to be removed
+   */
+  if (language) slug = slug.slice(1)
+  /**
+   * If the slug array is longer than 1
+   */
   if (slug.length < 2) return false
+  console.log('productSlug: ' + slug)
+  // Return false if matches product startpage
   return slug[0] === 'products' && slug[1] !== 'products'
 }
--- composables/ecommerce/isSingleProductCategory.js
+++ composables/ecommerce/isSingleProductCategory.js
@@ -1,5 +1,13 @@
 export default async function () {
-  const slug = await getSlug()
+  let slug = await getSlug()
+
+  const language = await getLanguage(slug)
+  /**
+   * If a specific language is requested, the first part of the slug (the language code) needs to be removed
+   */
+  if (language) slug = slug.slice(1)
+
   if (slug.length < 2) return false
+  // Return false if matches product-categories startpage
   return slug[0] === 'product-categories' && slug[1] !== 'product-categories'
 }
